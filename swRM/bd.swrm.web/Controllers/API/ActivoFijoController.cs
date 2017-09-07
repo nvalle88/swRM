9658ee26737d716ad384ad2aa6e40b629cb770d2
carlosavila890 <carlos.avila8909@gmail.com>
2017-09-07 12:24:13 -0400

    Creación del controlador API ActivoFijo y arreglo de la función Existe en ClaseActivoFijo

---
+++ swRM/bd.swrm.web/Controllers/API/ActivoFijoController.cs
@@ -0,0 +1,30 @@
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading.Tasks;
+using Microsoft.AspNetCore.Http;
+using Microsoft.AspNetCore.Mvc;
+using bd.swrm.datos;
+using bd.swrm.entidades.Negocio;
+using bd.log.guardar.Servicios;
+using bd.log.guardar.Enumeradores;
+using Microsoft.EntityFrameworkCore;
+using bd.log.guardar.ObjectTranfer;
+using bd.swrm.entidades.Enumeradores;
+using bd.log.guardar.Utiles;
+using bd.swrm.entidades.Utils;
+
+namespace bd.swrm.web.Controllers.API
+{
+    [Produces("application/json")]
+    [Route("api/ActivoFijo")]
+    public class ActivoFijoController : Controller
+    {
+        private readonly SwRMDbContext db;
+
+        public ActivoFijoController(SwRMDbContext db)
+        {
+            this.db = db;
+        }
+    }
+}
