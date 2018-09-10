dc77b78959334e64a94e96337c65a557642bdfa7
Carlos Avila <carlos.avila8909@gmail.com>
2018-09-10 13:51:38 -0400

    Reporte de proveeduría Movimientos de salida.

--- swRM/bd.swrm.entidades/ObjectTransfer/RangoFechaTransfer.cs
+++ swRM/bd.swrm.entidades/ObjectTransfer/RangoFechaTransfer.cs
@@ -9,4 +9,10 @@ namespace bd.swrm.entidades.ObjectTransfer
         public DateTime? FechaInicial { get; set; }
         public DateTime? FechaFinal { get; set; }
     }
+
+    public class RangoFechaEstadoTransfer
+    {
+        public RangoFechaTransfer RangoFechaTransfer { get; set; }
+        public List<string> Estados { get; set; }
+    }
 }
